42795d71ce49e23e93441bbc8ed35ddf2705bc9d
hyeonu kim <92966095+khw18@users.noreply.github.com>
2024-02-27 10:59:55 +0900

    [Bronze II] Title: 분해합, Time: 108 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -6,15 +6,16 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int sum;
 
         for (int i = 1; i <= n; i++) {
-            sum = 0;
-            String str = Integer.toString(i);
-            sum += i;
-            for (int j = 0; j < str.length(); j++) {
-                sum += str.charAt(j) - '0';
+            int num = i;
+            int sum = i;
+
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
             }
+
             if (sum == n) {
                 System.out.println(i);
                 return;
