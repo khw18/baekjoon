5b9134f2cbb4da4700ac27c61360ec5793652d6e
hyeonu kim <92966095+khw18@users.noreply.github.com>
2024-03-06 10:26:55 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 160 ms, Memory: 12676 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = N; i > 0; i--) {
+            for (int j = i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
