62cfa3fe6fb2f076b5624eeed71b207beda6849f
hyeonu kim <92966095+khw18@users.noreply.github.com>
2024-07-04 15:22:58 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 528 ms, Memory: 113012 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Main {
+    static int n, m;
+    static boolean[][] map;
+    static boolean[] visit;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new boolean[n + 1][n + 1];
+        visit = new boolean[n + 1];
+        int cnt = 0;
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            map[u][v] = true;
+            map[v][u] = true;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!visit[i]) {
+                dfs(i);
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+
+    public static void bfs(int s) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(s);
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            visit[cur] = true;
+            for (int i = 1; i <= n; i++) {
+                if (map[cur][i] && !visit[i]) {
+                    q.offer(i);
+                }
+            }
+        }
+    }
+
+    public static void dfs(int s) {
+        visit[s] = true;
+
+        for (int i = 1; i <= n; i++) {
+            if (map[s][i] && !visit[i]) {
+                dfs(i);
+            }
+        }
+    }
+
+}
