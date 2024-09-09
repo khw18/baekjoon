b1fa3fdb53e1845a6a44208c1967224882521175
hyeonu kim <92966095+khw18@users.noreply.github.com>
2024-09-09 20:55:14 +0900

    [Silver III] Title: 퇴사, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,29 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 class Main {
+    static int N, max = Integer.MIN_VALUE;
+    static int[] T, P;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        int[] T = new int[N];
-        int[] P = new int[N];
-        int[] dp = new int[N + 1];
+        N = Integer.parseInt(br.readLine());
+        T = new int[N + 1];
+        P = new int[N + 1];
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             T[i] = Integer.parseInt(st.nextToken());
             P[i] = Integer.parseInt(st.nextToken());
         }
-		for (int i = 0; i < N; i++) {
-			if(i+T[i]<=N)
-				dp[i + T[i]] = Math.max(dp[i + T[i]], dp[i] + P[i]);
-            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
-        }
-
-        System.out.println(dp[N]);
+        dfs(1, 0);
+        System.out.println(max);
     }
-	
+
+    static void dfs(int idx, int sum) {
+        if (idx == N + 1) {
+            max = Math.max(max, sum);
+            return;
+        }
+        if (idx > N) {
+            return;
+        }
+        if (idx + T[idx] <= N + 1) { // 그 날 상담 진행
+            dfs(idx + T[idx], sum + P[idx]);
+        }
+        dfs(idx + 1, sum); // 오늘 상담 스킵 -> 내일
+    }
 }
