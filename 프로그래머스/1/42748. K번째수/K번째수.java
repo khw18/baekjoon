075ff83c9cf9413fd17c5cb66d08a4dfd801bb66
hyeonu kim <92966095+khw18@users.noreply.github.com>
2024-10-11 21:54:40 +0900

    [level 1] Title: K번째수, Time: 0.41 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int k = 0;
+        for(int i = 0; i < commands.length; i++){
+            int[] arr = new int[commands[i][1] - commands[i][0] + 1];
+            int idx = 0;
+            for(int j = commands[i][0]-1; j < commands[i][1]; j++){
+            	arr[idx++] = array[j];
+            }
+        	Arrays.sort(arr);
+            answer[k++] = arr[commands[i][2]-1];
+        }
+        return answer;
+    }
+}
